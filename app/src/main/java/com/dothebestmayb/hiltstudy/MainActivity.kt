db90afc1593e99a756fbc533892e917ced99b384
DoDoBest <send1684@gmail.com>
2024-03-08 16:57:45 +0900

    feat: MainActivity에 MyName 변수를 Hilt를 이용해 주입받아 사용하기

--- app/src/main/java/com/dothebestmayb/hiltstudy/MainActivity.kt
+++ app/src/main/java/com/dothebestmayb/hiltstudy/MainActivity.kt
@@ -2,12 +2,19 @@ package com.dothebestmayb.hiltstudy
 
 import android.os.Bundle
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var myName: MyName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.text_view).text = myName.toString()
 
     }
 }
