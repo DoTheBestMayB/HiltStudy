bcbfe491aa45b0ac3d9ac1a9147bd1dfa6d6733e
DoDoBest <send1684@gmail.com>
2024-03-08 16:22:10 +0900

    feat: 프로젝트 생성 및 hilt 설정

---
+++ app/src/main/java/com/dothebestmayb/hiltstudy/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.dothebestmayb.hiltstudy
+
+import android.os.Bundle
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
