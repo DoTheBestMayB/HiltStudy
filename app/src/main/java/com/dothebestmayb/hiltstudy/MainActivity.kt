f8ca29b3b4a4508aabf3d067ed66957acd0ec157
DoDoBest <send1684@gmail.com>
2024-03-10 00:38:59 +0900

    feat: MainActivity에 Application을 주입받아 출력해봄으로써 default bindings 확인해보기

--- app/src/main/java/com/dothebestmayb/hiltstudy/MainActivity.kt
+++ app/src/main/java/com/dothebestmayb/hiltstudy/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.dothebestmayb.hiltstudy
 
+import android.app.Application
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,10 +14,14 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var myName: MyName
 
+    @Inject
+    lateinit var app: Application
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.e("MainActivity", "app = $app")
         findViewById<TextView>(R.id.text_view).text = myName.toString()
 
     }
