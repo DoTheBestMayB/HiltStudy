973f8c19a20fc2d799c47145507c45117dca64e3
DoDoBest <send1684@gmail.com>
2024-03-14 18:46:36 +0900

    feat: lazy 주입 해보기

--- app/src/main/java/com/dothebestmayb/hiltstudy/MainActivity.kt
+++ app/src/main/java/com/dothebestmayb/hiltstudy/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.dothebestmayb.hiltstudy
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import dagger.Lazy
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
-import javax.inject.Named
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     val TAG = MainActivity::class.java.simpleName
 
+    @Inject
+    lateinit var lazyFoo: Lazy<Foo>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        assert(this::lazyFoo.isInitialized)
+
+        val foo1: Foo = lazyFoo.get()
+        val foo2: Foo = lazyFoo.get()
+        assert(foo1 != null)
+        assert(foo1 === foo2)
     }
 }
