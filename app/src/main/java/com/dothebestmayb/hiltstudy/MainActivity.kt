8430f7aa1e290e69126c389dda77aa04ef41fb47
DoDoBest <send1684@gmail.com>
2024-03-14 18:55:38 +0900

    feat: Foo에 Scope를 지정해주지 않은 경우, 필드 주입되는 객체는 각각 다르다.

--- app/src/main/java/com/dothebestmayb/hiltstudy/MainActivity.kt
+++ app/src/main/java/com/dothebestmayb/hiltstudy/MainActivity.kt
@@ -12,17 +12,15 @@ class MainActivity : AppCompatActivity() {
     val TAG = MainActivity::class.java.simpleName
 
     @Inject
-    lateinit var lazyFoo: Lazy<Foo>
+    lateinit var lazyFoo1: Lazy<Foo>
+
+    @Inject
+    lateinit var lazyFoo2: Lazy<Foo>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        assert(this::lazyFoo.isInitialized)
-
-        val foo1: Foo = lazyFoo.get()
-        val foo2: Foo = lazyFoo.get()
-        assert(foo1 != null)
-        assert(foo1 === foo2)
+        assert(lazyFoo1 !== lazyFoo2)
     }
 }
