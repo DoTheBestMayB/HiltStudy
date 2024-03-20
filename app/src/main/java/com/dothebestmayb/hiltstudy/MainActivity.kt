542260e8a879b24c6d901861a87356c620c70469
DoDoBest <send1684@gmail.com>
2024-03-20 20:07:55 +0900

    feat: CustomKey Map 멀티 바인딩 사용해보기

--- app/src/main/java/com/dothebestmayb/hiltstudy/MainActivity.kt
+++ app/src/main/java/com/dothebestmayb/hiltstudy/MainActivity.kt
@@ -15,12 +15,14 @@ class MainActivity : AppCompatActivity() {
     lateinit var oneString: Set<String>
 
     @Inject
-    lateinit var map: Map<AnimalKey, String>
+    lateinit var map: Map<Animal, String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.e(TAG, map.toString())
+        Log.e(TAG, map[Animal.CAT].orEmpty())
+        Log.e(TAG, map[Animal.DOG].orEmpty())
+        Log.e(TAG, map[Animal.PIG].orEmpty()) // 바인딩 하지 않았기 때문에 빈 값이 출력된다.
     }
 }
