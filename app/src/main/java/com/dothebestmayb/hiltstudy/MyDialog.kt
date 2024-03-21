b738e5f53aafc31f18944941c1be22b46332c05c
DoDoBest <send1684@gmail.com>
2024-03-21 20:10:01 +0900

    feat: 커스텀 컴포넌트 사용해보기

--- app/src/main/java/com/dothebestmayb/hiltstudy/MyDialog.kt
+++ app/src/main/java/com/dothebestmayb/hiltstudy/MyDialog.kt
@@ -1,14 +1,14 @@
 package com.dothebestmayb.hiltstudy
 
+import android.app.Activity
 import android.app.Dialog
-import android.content.Context
 import android.os.Bundle
 import android.widget.TextView
 import dagger.hilt.EntryPoints
 import javax.inject.Inject
 
 class MyDialog @Inject constructor(
-    private val context: Context,
+    private val context: Activity,
     private val dialogComponentManager: DialogComponentManager
 ): Dialog(context) {
 
