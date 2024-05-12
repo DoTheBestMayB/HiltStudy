45d870d9de7ef1bb2f1611eca4c294ff3899d672
DoDoBest <send1684@gmail.com>
2024-05-12 19:27:27 +0900

    feat: SignUpScreen 구현

--- presentation/src/main/java/com/dothebestmayb/presentation/theme/Color.kt
+++ presentation/src/main/java/com/dothebestmayb/presentation/theme/Color.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 val primary = Color(0xFFD0BCFF)
 val primaryContainer = Color(0xFFCCC2DC)
+val background = Color(0XFF1C1C1C)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
